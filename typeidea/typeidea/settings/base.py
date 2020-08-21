0906c74c61cd117745a7b4ecf33f89d05f5086b4
yubatian <1260081619@qq.com>
2020-08-21 21:48:03 +0800

    add model

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,7 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'typeidea.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,14 @@
+from .base import *  # NOQA
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # 连接的数据库类型
+        'HOST': '192.168.25.3',  # 连接数据库的地址
+        'PORT': 3306,  # 端口
+        'NAME': 'typeidea',  # 数据库名称
+        'USER': 'root',  # 用户
+        'PASSWORD': 'Cmvcadmin2020!'  # 密码
+    }
+}
