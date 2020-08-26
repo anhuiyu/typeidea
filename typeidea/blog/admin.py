363461143f118d8707ca6503c840fb4742f9129c
yubatian <1260081619@qq.com>
2020-08-26 21:43:02 +0800

    the 6

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -4,6 +4,8 @@ from django.utils.html import format_html
 from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
 from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
+from django.contrib.admin.models import LogEntry
 
 # Register your models here.
 
@@ -12,25 +14,19 @@ class PostInline(admin.TabularInline):
     extra = 1
     model = Post
 @admin.register(Category,site=custom_site)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(BaseOwnerAdmin):
     inlines = [PostInline,]
-    list_display = ('name', 'status', 'is_nav', 'created_time')
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
     fields = ('name', 'status', 'is_nav')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(CategoryAdmin, self).save_model(request, obj, form, change)
-
+    def post_count(self,obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
 
 @admin.register(Tag,site=custom_site)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(TagAdmin, self).save_model(request, obj, form, change)
-
 
 class CategoryOwnerFilter(admin.SimpleListFilter):
     """自定义过滤只展示当前用户分类"""
@@ -44,7 +40,7 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
             return queryset.filter(category_id=self.value())
 
 @admin.register(Post,site=custom_site)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(BaseOwnerAdmin):
     form = PostAdminForm
     list_display = [
         'title', 'category', 'status', 'created_time', 'operator','owner'
@@ -54,6 +50,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title', 'category__name']
     actions_on_top = True
     actions_on_bottom = True
+
 
     # 编辑页面
     save_on_top = True
@@ -66,10 +63,7 @@ class PostAdmin(admin.ModelAdmin):
             reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = '操作'
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(PostAdmin, self).save_model(request, obj, form, change)
 
-    def get_queryset(self, request):
-        qs=super(PostAdmin,self).get_queryset(request)
-        return qs.filter(owner=request.user)
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr','object_id','action_flag','user','change_message']
