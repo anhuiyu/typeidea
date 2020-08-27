a1fd008ca058250254e493cbf4a603b8c5e1963b
Administrator <1260081619@qq.com>
2020-08-27 17:12:13 +0800

    7

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -22,6 +22,19 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    @classmethod
+    def get_navs(cls):
+        categories=cls.objects.filter(status=cls.STATUS_NORMAL)
+        nav_categories=[]
+        normal_categories=[]
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+        return {'navs':nav_categories,'categories':normal_categories}
+
+
 class Tag(models.Model):
     STATUS_NORMAL = 1
     STATUS_DELETE = 0
@@ -62,3 +75,28 @@ class Post(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id'] #根据id进行降序排列
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag=Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            tag=None
+            post_list=[]
+        else:
+            post_list=tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')
+        return post_list,tag
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category=Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category=None
+            post_list=[]
+        else:
+            post_list=category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')
+        return post_list,category
+    @classmethod
+    def latest_posts(cls):
+        post_list=cls.objects.filter(status=cls.STATUS_NORMAL)
+        return post_list
--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from .models import Post,Tag,Category
+from django.http import HttpResponse
 # Create your views here.
+
+def post_list(request, category_id=None,tag_id=None):
+    tag=None
+    category=None
+    if tag_id:
+        post_list,tag=Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list,category=Post.get_by_category(category_id)
+    else:
+        post_list=Post.latest_posts()
+    context= {
+        'post_list': post_list,
+        'category': category,
+        'tag': tag
+    }
+    context.update(Category.get_navs())
+    return render(request,'blog/list.html',context=context)
+def post_detail(request, post_id=None):
+    try:
+        post=Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post=None
+    context={
+        'post':post,
+    }
+    context.update(Category.get_navs())
+    return render(request,'blog/detail.html',context=context)
+
+
